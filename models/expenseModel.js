76f9b237b24ed1251ef4ba76d71396ecae629504
Ali Zaki <ali.mostafa.zaki@gmail.com>
2023-07-04 00:41:16 +0300

    Processing...

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -1,149 +1,39 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
+// const slugify = require('slugify');
 // const validator = require('validator');
 
-const expenseSchema = new mongoose.Schema(
-  {
+const expenseSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  details: {
     name: {
       type: String,
       required: [true, 'A expense must have a name'],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'A expense must have less or equal 40 characters'],
-      minlength: [10, 'A expense must have more or equal 10 characters'],
-      // validate: [validator.isAlpha, 'Expense name must only contain characters'],
     },
-    slug: String,
-    duration: {
+    quantity: {
       type: Number,
-      required: [true, 'A expense must have a duration'],
-    },
-    maxGroupSize: {
-      type: Number,
-      required: [true, 'A expense must have a group size'],
-    },
-    difficulty: {
-      type: String,
-      required: [true, 'A expense must have a difficulty'],
-      enum: {
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult',
-      },
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-      min: [1, 'A expense must be above 1.0'],
-      max: [5, 'A expense must be below 5.0'],
-      set: (val) => Math.round(val * 10) / 10,
-    },
-    ratingsQuantity: {
-      type: Number,
+      required: [true, 'A expense must have a quantity'],
       default: 0,
     },
-    price: {
-      type: Number,
-      required: [true, 'A expense must have a price'],
-    },
-    priceDiscount: {
-      type: Number,
-      validate: {
-        validator: function (val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: 'Discount price ({VALUE}) should be below regular price',
-      },
-    },
-    summary: {
-      type: String,
-      trim: true,
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    imageCover: {
-      type: String,
-      required: [true, 'A expense must have an cover image'],
-    },
-    images: [String],
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false,
-    },
-    startDates: [Date],
-    secretExpense: {
-      type: Boolean,
-      default: false,
-    },
-    startLocation: {
-      type: {
-        type: String,
-        default: 'Point',
-        enum: ['Point'],
-      },
-      coordinates: [Number],
-      address: String,
-      description: String,
-    },
-    locations: [
-      {
-        type: {
-          type: String,
-          default: 'Point',
-          enum: ['Point'],
-        },
-        coordinates: [Number],
-        address: String,
-        description: String,
-        day: Number,
-      },
-    ],
-    guides: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-      },
-    ],
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-
-expenseSchema.index({ price: 1, ratingsAverage: -1 });
-expenseSchema.index({ slug: 1 });
-expenseSchema.index({ startLocation: '2dsphere' });
-
-expenseSchema.virtual('durationWeeks').get(function () {
-  return this.duration / 7;
 });
 
-expenseSchema.virtual('reviews', {
-  ref: 'Review',
-  foreignField: 'expense',
-  localField: '_id',
-});
+// expenseSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
 
-// DOCUMENT MIDDLEWARE: runs before .save() and .create()
-expenseSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-// QUERY MIDDLEWARE
-expenseSchema.pre(/^find/, function (next) {
-  this.find({ secretExpense: { $ne: true } });
-  this.start = Date.now();
-  this.populate({
-    path: 'guides',
-    select: '-__v -passwordChangedAt',
-  });
-  next();
-});
+// expenseSchema.pre(/^find/, function (next) {
+//   this.find({ secretExpense: { $ne: true } });
+//   this.start = Date.now();
+//   this.populate({
+//     path: 'guides',
+//     select: '-__v -passwordChangedAt',
+//   });
+//   next();
+// });
 
 // expenseSchema.post(/^find/, function (docs, next) {
 //   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
--- models/itemModel.js
+++ models/itemModel.js
@@ -1,88 +1,33 @@
 const mongoose = require('mongoose');
-const Item = require('./expenseModel');
 
-const reviewSchema = new mongoose.Schema(
-  {
-    review: {
-      type: String,
-      required: [true, 'Review can not be empty!'],
-    },
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    item: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Item',
-      required: [true, 'Review must belong to a item.'],
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: [true, 'Review must belong to a user.'],
-    },
+const itemSchema = new mongoose.Schema({
+  item: {
+    type: String,
+    required: [true, 'Item can not be empty!'],
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-
-reviewSchema.index({ item: 1, user: 1 }, { unique: true });
-
-reviewSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: 'user',
-    select: 'name photo',
-  });
-  next();
+  car: {
+    type: String,
+    enum: ['Malibu', 'Equinox'],
+    required: [true, 'Spare Part can not be empty! Must belong to a Car'],
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  averagePrice: {
+    type: Number,
+    default: 0,
+  },
 });
 
-reviewSchema.statics.calcAverageRatings = async function (itemId) {
-  const stats = await this.aggregate([
-    {
-      $match: { item: itemId },
-    },
-    {
-      $group: {
-        _id: '$item',
-        nRating: { $sum: 1 },
-        avgRating: { $avg: '$rating' },
-      },
-    },
-  ]);
+// itemSchema.post('save', function () {
 
-  if (stats.length > 0) {
-    await Item.findByIdAndUpdate(itemId, {
-      ratingsQuantity: stats[0].nRating,
-      ratingsAverage: stats[0].avgRating,
-    });
-  } else {
-    await Item.findByIdAndUpdate(itemId, {
-      ratingsQuantity: 0,
-      ratingsAverage: 4.5,
-    });
-  }
-};
+// });
 
-reviewSchema.post('save', function () {
-  this.constructor.calcAverageRatings(this.item);
-});
+// itemSchema.pre(/^findOneAnd/, async function (next) {
+//   next();
+// });
 
-reviewSchema.pre(/^findOneAnd/, async function (next) {
-  this.r = await this.findOne();
-  next();
-});
+const Item = mongoose.model('Item', itemSchema);
 
-reviewSchema.post(/^findOneAnd/, async function () {
-  await this.r.constructor.calcAverageRatings(this.r.item);
-});
-
-const Review = mongoose.model('Review', reviewSchema);
-
-module.exports = Review;
+module.exports = Item;
